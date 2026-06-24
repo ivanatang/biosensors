13cf147e384cf2fff36e99a4662baa57dc160b22
Ivana Tang <ivana.tang@colorado.edu>
2026-06-24 15:09:01 -0600

    Merge xtnd scripts for all seq types into one script

--- xtnd_prod_PYR1_LCA.sh
+++ xtnd_prod_PYR1_LCA.sh
@@ -26,11 +26,27 @@ module load gromacs
 conda activate biosensors
 
 # Set some environment variables
-DIR=`pwd`
+DIR=/projects/ivta1597/biosensors
 MDP=$DIR/MDP
 
 # Get sequence value from command line
 ID=$1
+SEQ_TYPE=$2 # binders | nonbinders | neg_fail_gate | neg_low_pkt
+PREFIX=$3 # pair, bind
+
+if [ "$SEQ_TYPE" == "binders" ]; then
+    SUFFIX="binder"
+elif [ "$SEQ_TYPE" == "nonbinders" ]; then
+    SUFFIX="nb"
+elif [ "$SEQ_TYPE" == "neg_fail_gate" ]; then
+    SUFFIX="fail_gate"
+elif [ "$SEQ_TYPE" == "neg_low_pkt" ]; then
+    SUFFIX="low_pkt"
+else
+    echo "ERROR: Unknown SEQ_TYPE '$SEQ_TYPE'" >&2
+    exit 1
+fi
+
 PME=16
 RDD=1.2
 
@@ -40,14 +56,14 @@ D3=2
 
 # Production simulation
 # dodecahedron
-cd $DIR/binders/bind_${ID}_binder
+cd $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}
 cd prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}_${PME}PME_${D1}${D2}${D3}dd
 #cd prod_md_0p9_cutoff_3dt_64x1
 #mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK #-rdd $RDD -npme $PME
 mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK -npme $PME -dd $D1 $D2 $D3
 
 # cube
-#cd $DIR/binders/${SEQ}_binder/HMR
+#cd $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/HMR
 #cd prod_md_0p9_cutoff_3dt_64x1
 #mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK #-rdd $RDD -npme $PME
 
