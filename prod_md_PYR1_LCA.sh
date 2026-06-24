620f98d3aa441ff3a5de958cb0027651144f6fe0
Ivana Tang <ivana.tang@colorado.edu>
2026-06-24 15:05:07 -0600

    Merge prod scripts for all seq types into one script

--- prod_md_PYR1_LCA.sh
+++ prod_md_PYR1_LCA.sh
@@ -26,12 +26,27 @@ module load gromacs
 conda activate biosensors
 
 # Set some environment variables
-DIR=`pwd`
+DIR=/projects/ivta1597/biosensors
 MDP=$DIR/MDP
 
 # Get sequence value from command line
 ID=$1
-PREFIX=$2
+SEQ_TYPE=$2 # binders | nonbinders | neg_fail_gate | neg_low_pkt
+PREFIX=$3 # pair, bind
+
+if [ "$SEQ_TYPE" == "binders" ]; then
+    SUFFIX="binder"
+elif [ "$SEQ_TYPE" == "nonbinders" ]; then
+    SUFFIX="nb"
+elif [ "$SEQ_TYPE" == "neg_fail_gate" ]; then
+    SUFFIX="fail_gate"
+elif [ "$SEQ_TYPE" == "neg_low_pkt" ]; then
+    SUFFIX="low_pkt"
+else
+    echo "ERROR: Unknown SEQ_TYPE '$SEQ_TYPE'" >&2
+    exit 1
+fi
+
 PME=16
 RDD=1.2
 
@@ -40,15 +55,15 @@ D2=4
 D3=2
 
 # Production simulation - HMR (dodecahedron)
-cd $DIR/binders/${PREFIX}_${ID}_binder
+cd $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}
 mkdir prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}_${PME}PME_${D1}${D2}${D3}dd
 cd prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}_${PME}PME_${D1}${D2}${D3}dd
-gmx_mpi grompp -f $MDP/prod_md_HMR_3dt.mdp -c $DIR/binders/${PREFIX}_${ID}_binder/NPT/npt.gro -t $DIR/binders/${PREFIX}_${ID}_binder/NPT/npt.cpt -p $DIR/binders/${PREFIX}_${ID}_binder/${PREFIX}_${ID}_dodecahedron_HMR.top -o prod_md_500ns.tpr
+gmx_mpi grompp -f $MDP/prod_md_HMR_3dt.mdp -c $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/NPT/npt.gro -t $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/NPT/npt.cpt -p $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/${PREFIX}_${ID}_dodecahedron_HMR.top -o prod_md_500ns.tpr
 mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -ntomp $SLURM_CPUS_PER_TASK -npme $PME -dd $D1 $D2 $D3
 
 # Production simulation - HMR (cube)
-#cd $DIR/binders/${SEQ}_binder/HMR
+#cd $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/HMR
 #mkdir prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}
 #cd prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}
-#gmx_mpi grompp -f $MDP/prod_md_HMR_3dt.mdp -c $DIR/binders/${SEQ}_binder/HMR/NPT/npt.gro -t $DIR/binders/${SEQ}_binder/HMR/NPT/npt.cpt -p $DIR/binders/${SEQ}_binder/HMR/${SEQ}_b_HMR.top -o prod_md_500ns.tpr
+#gmx_mpi grompp -f $MDP/prod_md_HMR_3dt.mdp -c $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/HMR/NPT/npt.gro -t $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/HMR/NPT/npt.cpt -p $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/HMR/${SEQ}_b_HMR.top -o prod_md_500ns.tpr
 #mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -ntomp $SLURM_CPUS_PER_TASK #-rdd $RDD -npme $PME
