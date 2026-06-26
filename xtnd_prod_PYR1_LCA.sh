37dc34e94d916580f52e55fc83e42e9569519434
Ivana Tang <ivana.tang@colorado.edu>
2026-06-25 20:48:38 -0600

    Fix path to find trajectory

--- xtnd_prod_PYR1_LCA.sh
+++ xtnd_prod_PYR1_LCA.sh
@@ -31,6 +31,7 @@ conda activate biosensors
 # Set some environment variables
 DIR=/projects/ivta1597/biosensors
 MDP=$DIR/MDP
+BASE=/scratch/alpine/ivta1597/LCA_boltz_models
 
 # Get sequence value from command line
 ID=$1
@@ -57,16 +58,14 @@ D1=6
 D2=4
 D3=2
 
-# Production simulation
-# dodecahedron
-cd $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}
-cd prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}_${PME}PME_${D1}${D2}${D3}dd
-#cd prod_md_0p9_cutoff_3dt_64x1
-#mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK #-rdd $RDD -npme $PME
-mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK -npme $PME -dd $D1 $D2 $D3
+# Navigate to the sequence's scratch directory for trajectory files
+SEQ_DIR=$BASE/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}
+if [ ! -d "$SEQ_DIR" ]; then
+    echo "ERROR: Sequence directory not found: $SEQ_DIR" >&2
+    exit 1
+fi
 
-# cube
-#cd $DIR/${SEQ_TYPE}/${PREFIX}_${ID}_${SUFFIX}/HMR
-#cd prod_md_0p9_cutoff_3dt_64x1
-#mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK #-rdd $RDD -npme $PME
+cd "$SEQ_DIR"
+cd prod_md_0p9_cutoff_3dt_${SLURM_NTASKS}x${SLURM_CPUS_PER_TASK}_${PME}PME_${D1}${D2}${D3}dd
+mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm prod_md_500ns -s prod_md_500ns.tpr -cpi prod_md_500ns.cpt -append -ntomp $SLURM_CPUS_PER_TASK -npme $PME -dd $D1 $D2 $D3
 
